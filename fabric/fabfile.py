7d62b3a1bb65c0d9f563f8d7c18bfbb58bec8ab0
Johnson Li <johnsonli1993@gmail.com>
2017-12-26 16:59:00 +0800

    generate ssh config

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,5 +1,14 @@
-from fabric.api import run
+import os
+
+from fabric.api import run, env, sudo
+
+env.use_ssh_config = True
+env.ssh_config_path = os.path.dirname(os.path.dirname(__file__)) + '/resources/ssh/config'
 
 
 def host_type():
-    run('uname -s')
+    run('uname -a')
+
+
+def setup_bridge():
+    sudo('whoami')
--- hestia/tools/generate_hosts.py
+++ hestia/tools/generate_hosts.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sqlite3
-import os
 
 from hestia import RESOURCE_PATH, ANSIBLE_PATH
 from hestia.aws.regions import REGIONS
@@ -9,9 +8,10 @@ from hestia.aws.regions import REGIONS
 DB_PATH = RESOURCE_PATH + '/db'
 DB_FILE = DB_PATH + '/instances.db'
 HOSTS_FILE = ANSIBLE_PATH + '/inventory/hosts.yml'
+CONFIG_FILE = RESOURCE_PATH + '/ssh/config'
 
 
-def main():
+def generate_ansible_hosts():
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
     open(HOSTS_FILE, 'w').close()
@@ -35,5 +35,21 @@ def main():
     f.close()
 
 
+def generate_ssh_hosts():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    open(CONFIG_FILE, 'w').close()
+    f = open(CONFIG_FILE, 'a')
+    c.execute("SELECT * FROM instances")
+    for record in c.fetchall():
+        record = dict(zip([d[0] for d in c.description], record))
+        f.write('Host {}-{}\n'.format(REGIONS[record['region']].lower(), record['name']))
+        f.write('  HostName {}\n'.format(record['primaryIpv4Pub']))
+        f.write('  User ubuntu\n')
+        f.write('  IdentityFile ~/keys/{}.pem\n'.format(REGIONS[record['region']].lower()))
+        f.write('\n')
+
+
 if __name__ == '__main__':
-    main()
+    # generate_ansible_hosts()
+    generate_ssh_hosts()
