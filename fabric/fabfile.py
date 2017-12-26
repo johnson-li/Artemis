5b1ba4421bf0dd9b46f3fd5293ab06ab9037832e
Johnson <johnsonli1993@gmail.com>
2017-12-26 10:18:39 +0000

    hosts

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,11 +1,15 @@
 import os
 
-from fabric.api import run, env, sudo
+from fabric.api import run, env, sudo, parallel
 
 env.use_ssh_config = True
 env.ssh_config_path = os.path.dirname(os.path.dirname(__file__)) + '/resources/ssh/config'
 
+env.hosts = ['virginia-router', 'virginia-server', 'ohio-server', 'ohio-router', 'california-router', 'california-server', 'oregon-server', 'oregon-router', 'mumbai-server', 'mumbai-router', 'singapore-router', 'singapore-server', 'sydney-router', 'sydney-server', 'tokyo-router', 'tokyo-server', 'seoul-router', 'seoul-server', 'central-router', 'central-server', 'frankfurt-router', 'frankfurt-server', 'ireland-router', 'ireland-server', 'london-server', 'london-router',
+        'paris-server', 'paris-router', 'saopaulo-server', 'saopaulo-router']
 
+
+@parallel(pool_size=4)
 def host_type():
     run('uname -a')
 
