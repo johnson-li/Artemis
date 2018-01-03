3cacfa6e4d942a0e1357546c19633dc58875982a
Johnson Li <johnsonli1993@gmail.com>
2018-01-03 23:17:39 +0800

    update sync_server.sh

--- scripts/sync_servers.sh
+++ scripts/sync_servers.sh
@@ -3,8 +3,8 @@
 for host in `grep '\-router' ../resources/ssh/config| cut -d' ' -f2`
 do
     echo sync ${host}
-    ssh -F ../resources/ssh/config 'mkdir ~/Workspace/server'
-    scp -F ../resources/ssh/config ../udp-server/client ${host}:~/Workspace/server/
-    scp -F ../resources/ssh/config ../udp-server/dns_server ${host}:~/Workspace/server/
-    scp -F ../resources/ssh/config ../udp-server/serviceid_server ${host}:~/Workspace/server/
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -F ../resources/ssh/config ${host} 'mkdir ~/Workspace/server'
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -F ../resources/ssh/config ../udp-server/client ${host}:~/Workspace/server/
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -F ../resources/ssh/config ../udp-server/dns_server ${host}:~/Workspace/server/
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -F ../resources/ssh/config ../udp-server/serviceid_server ${host}:~/Workspace/server/
 done
