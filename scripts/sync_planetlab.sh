6bbcfba150b92ae50e588eb2022022a9f3a0bbaf
Johnson Li <johnsonli1993@gmail.com>
2018-01-04 21:06:29 +0800

    update sync_planetlab.sh

--- scripts/sync_planetlab.sh
+++ scripts/sync_planetlab.sh
@@ -5,7 +5,9 @@ BASE_DIR=$(dirname "$0")
 
 cd ${BASE_DIR}
 
-scp -r server goettingenple_txp1@$1:~/server
+ssh goettingenple_txp1@$1 'mkdir ~/server'
+scp server/client goettingenple_txp1@$1:~/server/client
+scp server/simple_client goettingenple_txp1@$1:~/server/simple_client
 scp conduct_exp1.sh goettingenple_txp1@$1:~/
 
 cd ${USER_DIR}
