4e8a821b41fff3a2e7f33669642e2fbd657ef61b
Johnson <johnsonli1993@gmail.com>
2018-01-04 12:42:55 +0000

    fix shell script

--- scripts/prepare_and_run.sh
+++ scripts/prepare_and_run.sh
@@ -4,7 +4,7 @@ host=$1
 ip=`dig +short ${host}`
 sid_server=`sqlite3 ../resources/db/sip.db "select server from sip where host = '${ip}'"`
 
-if [ -z ${ip} ]
+if [[ -z ${ip} ]]
 then
     cd ..
     python -m hestia.experiment.main ${ip}
