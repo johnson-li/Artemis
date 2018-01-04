bc71153ba7ed1ec30af6fe1eec349ac8ae08879a
Johnson Li <johnsonli1993@gmail.com>
2018-01-04 20:27:46 +0800

    update prepare_and_run script

--- scripts/prepare_and_run.sh
+++ scripts/prepare_and_run.sh
@@ -2,10 +2,14 @@
 
 host=$1
 ip=`dig +short ${host}`
+sid_server=`sqlite3 ../resources/db/sip.db "select server from sip where host = '${ip}'"`
 
-cd ..
-python -m hestia.experiment.main ${ip}
-cd scripts
+if [ -z ${ip} ]
+then
+    cd ..
+    python -m hestia.experiment.main ${ip}
+    cd scripts
+fi
 
 ./sync_planetlab.sh ${host}
 ./run_remote.sh ${ip}
