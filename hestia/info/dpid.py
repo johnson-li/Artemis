58595163f625f960cc4c28939367726428517516
Johnson Li <johnsonli1993@gmail.com>
2018-01-10 01:01:52 +0800

    update hosts info

--- hestia/info/dpid.py
+++ hestia/info/dpid.py
@@ -11,4 +11,52 @@ DPID = {
         'router': '00:00:f6:83:8e:3e:fa:40',
         'server': '00:00:4a:3e:54:de:b9:4a',
     },
+    'saopaulo': {
+        'router': '00:00:8e:e3:7d:97:63:4d',
+        'server': '00:00:06:4a:89:40:9f:4f',
+    },
+    'paris': {
+        'router': '00:00:52:5f:be:c5:6a:48',
+        'server': '00:00:7a:89:67:4a:9b:42',
+    },
+    'london': {
+        'router': '00:00:f2:ea:45:c6:96:4f',
+        'server': '00:00:86:fd:4e:90:a0:44',
+    },
+    'ireland': {
+        'router': '00:00:4e:7c:a9:36:ce:48',
+        'server': '00:00:06:af:a6:1b:78:48',
+    },
+    'frankfurt': {
+        'router': '00:00:d2:25:20:8e:5d:43',
+        'server': '00:00:26:78:01:62:28:40',
+    },
+    'central': {
+        'router': '00:00:ea:bb:4f:f6:3b:47',
+        'server': '00:00:16:ef:20:d0:56:45',
+    },
+    'seoul': {
+        'router': '00:00:06:b3:b5:73:75:4f',
+        'server': '00:00:1a:ea:e5:95:24:4b',
+    },
+    'mumbai': {
+        'router': '00:00:de:1a:b2:b3:a4:40',
+        'server': '00:00:46:c1:1f:de:6e:4a',
+    },
+    'oregon': {
+        'router': '00:00:06:5f:9a:f7:f5:40',
+        'server': '00:00:4a:f5:3a:a8:cc:42',
+    },
+    'california': {
+        'router': '00:00:86:9d:63:7b:61:4e',
+        'server': '00:00:26:94:be:6a:81:4c',
+    },
+    'ohio': {
+        'router': '00:00:6a:88:26:41:8f:4b',
+        'server': '00:00:5a:28:77:99:11:4b',
+    },
+    'virginia': {
+        'router': '00:00:5a:05:74:31:0e:41',
+        'server': '00:00:36:17:74:f6:4f:47',
+    },
 }
