37a7b65873b0db43fe3a19d96438ce8b43d0f408
Johnson Li <johnsonli1993@gmail.com>
2018-01-10 17:07:30 +0800

    fix mac.py

--- hestia/info/mac.py
+++ hestia/info/mac.py
@@ -16,62 +16,62 @@ MAC = {
     },
     'saopaulo': {
         'server': {
-            'br1' '06:4a:89:40:9f:4f'
+            'br1': '06:4a:89:40:9f:4f'
         }
     },
     'paris': {
         'server': {
-            'br1' '7a:89:67:4a:9b:42'
+            'br1': '7a:89:67:4a:9b:42'
         }
     },
     'london': {
         'server': {
-            'br1' '86:fd:4e:90:a0:44'
+            'br1': '86:fd:4e:90:a0:44'
         }
     },
     'ireland': {
         'server': {
-            'br1' '06:af:a6:1b:78:48'
+            'br1': '06:af:a6:1b:78:48'
         }
     },
     'frankfurt': {
         'server': {
-            'br1' '26:78:01:62:28:40'
+            'br1': '26:78:01:62:28:40'
         }
     },
     'central': {
         'server': {
-            'br1' '16:ef:20:d0:56:45'
+            'br1': '16:ef:20:d0:56:45'
         }
     },
     'seoul': {
         'server': {
-            'br1' '1a:ea:e5:95:24:4b'
+            'br1': '1a:ea:e5:95:24:4b'
         }
     },
     'mumbai': {
         'server': {
-            'br1' '46:c1:1f:de:6e:4a'
+            'br1': '46:c1:1f:de:6e:4a'
         }
     },
     'oregon': {
         'server': {
-            'br1' '4a:f5:3a:a8:cc:42'
+            'br1': '4a:f5:3a:a8:cc:42'
         }
     },
     'california': {
         'server': {
-            'br1' '26:94:be:6a:81:4c'
+            'br1': '26:94:be:6a:81:4c'
         }
     },
     'ohio': {
         'server': {
-            'br1' '5a:28:77:99:11:4b'
+            'br1': '5a:28:77:99:11:4b'
         }
     },
     'virginia': {
         'server': {
-            'br1' '36:17:74:f6:4f:47'
+            'br1': '36:17:74:f6:4f:47'
         }
     },
 }
