3f4b871c96cbcdcd4bc8b63a9d206856ff903cbb
Johnson Li <johnsonli1993@gmail.com>
2018-02-04 23:08:45 +0200

    gcp mac

--- hestia/info/mac.py
+++ hestia/info/mac.py
@@ -74,4 +74,12 @@ MAC = {
             'br1': '36:17:74:f6:4f:47'
         }
     },
+    'us-central1-c': {'server': {'br1': 'a6:99:21:1f:28:4c'}},
+    'us-east4-c': {'server': {'br1': '46:4e:66:cf:84:48'}},
+    'us-west1-c': {'server': {'br1': '72:d4:a5:16:29:46'}},
+    'us-east1-c': {'server': {'br1': 'ca:9f:e8:4c:9d:42'}},
+    'europe-west4-c': {'server': {'br1': 'ae:30:ce:67:71:43'}},
+    'europe-west3-c': {'server': {'br1': '7a:bf:87:52:26:4d'}},
+    'europe-west2-c': {'server': {'br1': '2e:f1:8f:a3:89:49'}},
+    'europe-west1-c': {'server': {'br1': 'd6:4e:1c:ca:8f:4f'}}
 }
--- hestia/tools/main.py
+++ hestia/tools/main.py
@@ -1,37 +1,19 @@
 def main():
     data = {
-        'us-central1-c-router': '000072a97e23cc43',
-        'us-central1-c-server': '0000a699211f284c',
-        'us-east4-c-router': '00003aa7191e2a47',
-        'us-east4-c-server': '0000464e66cf8448',
-        'us-west1-c-server': '000072d4a5162946',
-        'us-west1-c-router': '0000f248ac425943',
-        'us-east1-c-router': '0000c659f7869e48',
-        'us-east1-c-server': '0000ca9fe84c9d42',
-        'europe-west4-c-server': '0000ae30ce677143',
-        'europe-west4-c-router': '000046a66c222f4e',
-        'europe-west3-c-server': '00007abf8752264d',
-        'europe-west3-c-router': '00002e9a4ae4174c',
-        'europe-west2-c-server': '00002ef18fa38949',
-        'europe-west2-c-router': '0000964cf934e943',
-        'europe-west1-c-server': '0000d64e1cca8f4f',
-        'europe-west1-c-router': '0000aade75682749',
+        'us-central1-c-server': 'a6:99:21:1f:28:4c',
+        'us-east4-c-server': '46:4e:66:cf:84:48',
+        'us-west1-c-server': '72:d4:a5:16:29:46',
+        'us-east1-c-server': 'ca:9f:e8:4c:9d:42',
+        'europe-west4-c-server': 'ae:30:ce:67:71:43',
+        'europe-west3-c-server': '7a:bf:87:52:26:4d',
+        'europe-west2-c-server': '2e:f1:8f:a3:89:49',
+        'europe-west1-c-server': 'd6:4e:1c:ca:8f:4f',
     }
     res = {}
     for key, val in data.items():
         k1 = key[-6:]
         k2 = key[: -7]
-        if k2 not in res:
-            res[k2] = {}
-        dd = ''
-        count = 0
-        for c in val:
-            dd = dd + c
-            count += 1
-            if count == 2:
-                dd = dd + ':'
-                count = 0
-        res[k2][k1] = dd[:-1]
+        res[k2] = {'server': {'br1': val}}
     print(res)
 
 
