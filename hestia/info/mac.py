e661cd165cc4b458bdcec1e20819dc53dbd8f6d7
Johnson Li <johnsonli1993@gmail.com>
2018-02-12 00:17:51 +0200

    mac addresses

--- hestia/info/mac.py
+++ hestia/info/mac.py
@@ -26,62 +26,98 @@ MAC = {
     'saopaulo': {
         'server': {
             'br1': '06:4a:89:40:9f:4f'
-        }
+        },
+        'router': {
+            'eth1': ''
+        },
     },
     'paris': {
         'server': {
             'br1': '7a:89:67:4a:9b:42'
-        }
+        },
+        'router': {
+            'eth1': ''
+        },
     },
     'london': {
         'server': {
             'br1': '86:fd:4e:90:a0:44'
-        }
+        },
+        'router': {
+            'eth1': '06:40:45:89:9b:22'
+        },
     },
     'ireland': {
         'server': {
             'br1': '06:af:a6:1b:78:48'
-        }
+        },
+        'router': {
+            'eth1': '0a:7a:64:4d:f0:c8'
+        },
     },
     'frankfurt': {
         'server': {
             'br1': '26:78:01:62:28:40'
-        }
+        },
+        'router': {
+            'eth1': '06:14:08:55:eb:0c'
+        },
     },
     'central': {
         'server': {
             'br1': '16:ef:20:d0:56:45'
-        }
+        },
+        'router': {
+            'eth1': '02:ee:05:f9:a6:8c'
+        },
     },
     'seoul': {
         'server': {
             'br1': '1a:ea:e5:95:24:4b'
-        }
+        },
+        'router': {
+            'eth1': '02:b1:aa:0e:29:86'
+        },
     },
     'mumbai': {
         'server': {
             'br1': '46:c1:1f:de:6e:4a'
-        }
+        },
+        'router': {
+            'eth1': '02:d0:f4:7c:16:62'
+        },
     },
     'oregon': {
         'server': {
             'br1': '4a:f5:3a:a8:cc:42'
-        }
+        },
+        'router': {
+            'eth1': '02:91:dc:8c:e7:58'
+        },
     },
     'california': {
         'server': {
             'br1': '26:94:be:6a:81:4c'
-        }
+        },
+        'router': {
+            'eth1': '02:a2:7a:6e:8b:20'
+        },
     },
     'ohio': {
         'server': {
             'br1': '5a:28:77:99:11:4b'
-        }
+        },
+        'router': {
+            'eth1': '06:d6:12:cc:32:f4'
+        },
     },
     'virginia': {
         'server': {
             'br1': '36:17:74:f6:4f:47'
-        }
+        },
+        'router': {
+            'eth1': '12:59:54:7c:dc:a6'
+        },
     },
     'us-central1-c': {'server': {'br1': 'a6:99:21:1f:28:4c'}},
     'us-east4-c': {'server': {'br1': '46:4e:66:cf:84:48'}},
