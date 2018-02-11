ed8b23295ec4cf4ca136ec1cc5929402f6160efb
Johnson Li <johnsonli1993@gmail.com>
2018-02-11 23:31:17 +0200

    compile 32 machines

--- udp-server/compile.sh
+++ udp-server/compile.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
 gcc dns_server.c -o dns_server
 gcc serviceid_server.c -o serviceid_server
 gcc client.c -o client
 gcc simple_client.c -o simple_client
+gcc -m32 client.c -o i686/client
+gcc -m32 simple_client.c -o i686/simple_client
 
