af27467ab3aaa7a7ed74ce8e38f2b6bc54864b29
Johnson Li <johnsonli1993@gmail.com>
2018-02-12 02:51:54 +0200

    update mac

--- hestia/info/__init__.py
+++ hestia/info/__init__.py
@@ -1,7 +1,7 @@
 import re
 
 
-def main():
+def dpid():
     data = {}
     for line in open('/Users/johnson/output'):
         if 'OFPT_FEATURES_REPLY' in line:
@@ -10,6 +10,21 @@ def main():
             data[groups[0].lower()][groups[1]] = ':'.join(
                 [groups[2][i * 2: i * 2 + 2] for i in range(int(len(groups[2]) / 2))])
     print(data)
+
+
+def mac():
+    data = {}
+    for line in open('/Users/johnson/output'):
+        if 'Ethernet' in line:
+            groups = re.match('\[([a-z]+)-([a-z]+)\].*out: ([0-9a-z]+).*HWaddr ([0-9a-z:]+)', line.strip()).groups()
+            data.setdefault(groups[0].lower(), {})
+            data[groups[0].lower()].setdefault(groups[1], {})
+            data[groups[0]][groups[1]][groups[2]] = groups[3]
+    print(data)
+
+
+def main():
+    mac()
 
 
 if __name__ == '__main__':
--- hestia/info/mac.py
+++ hestia/info/mac.py
@@ -1,124 +1,34 @@
 MAC = {
-    'tokyo': {
-        'server': {
-            'br1': '1e:89:80:a8:96:4c'
-        },
-        'router': {
-            'eth1': '06:1e:a3:e4:1f:12'
-        }
-    },
-    'sydney': {
-        'server': {
-            'br1': '2a:ad:f0:91:54:4c'
-        },
-        'router': {
-            'eth1': '02:9c:0a:ca:a1:60'
-        }
-    },
-    'singapore': {
-        'server': {
-            'br1': '4a:3e:54:de:b9:4a'
-        },
-        'router': {
-            'eth1': '02:a6:f5:64:c6:50'
-        }
-    },
-    'saopaulo': {
-        'server': {
-            'br1': '06:4a:89:40:9f:4f'
-        },
-        'router': {
-            'eth1': ''
-        },
-    },
-    'paris': {
-        'server': {
-            'br1': '7a:89:67:4a:9b:42'
-        },
-        'router': {
-            'eth1': ''
-        },
-    },
-    'london': {
-        'server': {
-            'br1': '86:fd:4e:90:a0:44'
-        },
-        'router': {
-            'eth1': '06:40:45:89:9b:22'
-        },
-    },
-    'ireland': {
-        'server': {
-            'br1': '06:af:a6:1b:78:48'
-        },
-        'router': {
-            'eth1': '0a:7a:64:4d:f0:c8'
-        },
-    },
-    'frankfurt': {
-        'server': {
-            'br1': '26:78:01:62:28:40'
-        },
-        'router': {
-            'eth1': '06:14:08:55:eb:0c'
-        },
-    },
-    'central': {
-        'server': {
-            'br1': '16:ef:20:d0:56:45'
-        },
-        'router': {
-            'eth1': '02:ee:05:f9:a6:8c'
-        },
-    },
-    'seoul': {
-        'server': {
-            'br1': '1a:ea:e5:95:24:4b'
-        },
-        'router': {
-            'eth1': '02:b1:aa:0e:29:86'
-        },
-    },
-    'mumbai': {
-        'server': {
-            'br1': '46:c1:1f:de:6e:4a'
-        },
-        'router': {
-            'eth1': '02:d0:f4:7c:16:62'
-        },
-    },
-    'oregon': {
-        'server': {
-            'br1': '4a:f5:3a:a8:cc:42'
-        },
-        'router': {
-            'eth1': '02:91:dc:8c:e7:58'
-        },
-    },
-    'california': {
-        'server': {
-            'br1': '26:94:be:6a:81:4c'
-        },
-        'router': {
-            'eth1': '02:a2:7a:6e:8b:20'
-        },
-    },
-    'ohio': {
-        'server': {
-            'br1': '5a:28:77:99:11:4b'
-        },
-        'router': {
-            'eth1': '06:d6:12:cc:32:f4'
-        },
-    },
-    'virginia': {
-        'server': {
-            'br1': '36:17:74:f6:4f:47'
-        },
-        'router': {
-            'eth1': '12:59:54:7c:dc:a6'
-        },
-    },
+    'california': {'server': {'br1': '06:f5:fc:16:75:40', 'eth1': '02:01:91:23:20:50'},
+                   'router': {'br1': '7a:40:ea:e1:c7:4f', 'eth1': '02:a2:7a:6e:8b:20'}},
+    'ohio': {'server': {'br1': '12:0a:7f:4b:9d:48', 'eth1': '06:58:62:a3:d8:d2'},
+             'router': {'br1': 'ae:db:e8:5a:52:40', 'eth1': '06:d6:12:cc:32:f4'}},
+    'oregon': {'router': {'br1': '76:cb:b3:95:ae:4f', 'eth1': '02:91:dc:8c:e7:58'},
+               'server': {'br1': '6a:9d:cd:3e:61:47', 'eth1': '02:24:76:70:9d:a8'}},
+    'virginia': {'router': {'br1': 'da:82:b1:a3:cd:40', 'eth1': '12:59:54:7c:dc:a6'},
+                 'server': {'br1': 'e6:a8:58:72:4a:4c', 'eth1': '12:6a:29:a2:18:a6'}},
+    'london': {'server': {'br1': '92:58:17:3d:02:46', 'eth1': '06:98:ca:4d:3b:e6'},
+               'router': {'br1': '76:33:a2:f3:80:48', 'eth1': '06:40:45:89:9b:22'}},
+    'paris': {'server': {'br1': 'fe:1f:86:e4:d8:44', 'eth1': '06:f9:ac:28:50:a2'},
+              'router': {'br1': 'ba:b6:69:52:89:43', 'eth1': '06:8c:f1:e7:81:48'}},
+    'ireland': {'server': {'br1': '1e:53:da:62:b1:4d', 'eth1': '0a:4e:2c:96:83:9a'},
+                'router': {'br1': 'c2:46:76:4d:c7:4d', 'eth1': '0a:7a:64:4d:f0:c8'}},
+    'saopaulo': {'server': {'br1': 'd2:e8:ed:05:8e:47', 'eth1': '02:03:eb:f6:17:6e'},
+                 'router': {'br1': '42:50:45:cd:e5:4c', 'eth1': '02:d3:37:9f:6e:b2'}},
+    'central': {'server': {'br1': '32:87:67:bb:e9:44', 'eth1': '02:12:bd:8b:88:b6'},
+                'router': {'br1': '26:8a:7d:c7:0f:41', 'eth1': '02:ee:05:f9:a6:8c'}},
+    'frankfurt': {'server': {'br1': 'fe:1f:3c:3e:3b:46', 'eth1': '06:b1:c9:a3:4f:7e'},
+                  'router': {'br1': '8a:02:d3:7d:85:44', 'eth1': '06:14:08:55:eb:0c'}},
+    'sydney': {'server': {'br1': '66:7e:7a:bc:1b:4a', 'eth1': '02:07:30:b4:c9:6c'},
+               'router': {'br1': '12:2f:9d:59:7e:42', 'eth1': '02:9c:0a:ca:a1:60'}},
+    'singapore': {'server': {'br1': '9e:ad:9d:e6:eb:43', 'eth1': '02:bd:25:4e:af:5e'},
+                  'router': {'br1': '5a:98:0f:23:17:44', 'eth1': '02:a6:f5:64:c6:50'}},
+    'seoul': {'server': {'br1': 'ba:de:38:7b:ae:4f', 'eth1': '02:d6:1b:40:e5:b2'},
+              'router': {'br1': 'e2:78:4b:a4:3f:44', 'eth1': '02:b1:aa:0e:29:86'}},
+    'mumbai': {'server': {'br1': '7e:36:28:62:4a:42', 'eth1': '02:d1:2e:95:19:14'},
+               'router': {'br1': '1e:cc:7f:63:dd:42', 'eth1': '02:d0:f4:7c:16:62'}},
+    'tokyo': {'server': {'br1': 'c2:81:1f:46:c5:4a', 'eth1': '06:5a:89:7f:aa:4e'},
+              'router': {'br1': '72:0b:5c:92:01:4a', 'eth1': '06:1e:a3:e4:1f:12'}},
     'us-central1-c': {'server': {'br1': 'a6:99:21:1f:28:4c'}},
     'us-east4-c': {'server': {'br1': '46:4e:66:cf:84:48'}},
     'us-west1-c': {'server': {'br1': '72:d4:a5:16:29:46'}},
