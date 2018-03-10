b88bb846033cc9bd9868e82f25edbe4a76beec72
Johnson Li <johnsonli1993@gmail.com>
2018-03-10 10:10:06 +0200

    update conduct_exp1_sid.sh

--- scripts/conduct_exp1_sid.sh
+++ scripts/conduct_exp1_sid.sh
@@ -12,7 +12,7 @@ then
 fi
 echo repeat: ${repeat}
 
-sid_router=`sudo ping sid.xuebing.name -w2|grep PING| head -n1| egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'`
+sid_router=`sudo ping cdn.xuebing.name -w2|grep PING| head -n1| egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'`
 for i in `seq ${repeat}`
 do
     output=`sudo ./server/client ${sid_router} ${sid_server}| grep cost| egrep -o '[0-9.]+'`
