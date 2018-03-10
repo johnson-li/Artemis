c38a138c0cef1a42aa63ba9acc0b145bc054250d
Johnson <johnsonli1993@gmail.com>
2018-03-10 05:15:53 +0000

    update server

--- udp-server/serviceid_server.c
+++ udp-server/serviceid_server.c
@@ -61,17 +61,21 @@ int main(int argc, char **argv) {
         recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *) &remaddr, &addrlen);
         char str[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &(remaddr.sin_addr), str, INET_ADDRSTRLEN);
-        printf("received %zd bytes from %s:%d\n", recvlen, str, remaddr.sin_port);
+        printf("received %zd bytes from %s:%d\n", recvlen, str, ntohs(remaddr.sin_port));
         if (recvlen > 10) {
             printf("got message from gre\n");
             struct sockaddr_in remaddr_old;
             struct map_entry *entry;
             HASH_FIND_INT(map, &(remaddr.sin_addr.s_addr), entry);
-            remaddr_old = entry->val;
+            if (entry) {
+                remaddr_old = entry->val;
+            } else {
+                remaddr_old = remaddr;
+            }
             inet_ntop(AF_INET, &(remaddr_old.sin_addr), str, INET_ADDRSTRLEN);
             result = sendto(fd, send_buf, sizeof(send_buf) / sizeof(send_buf[0]), 0, (struct sockaddr *) &remaddr_old,
                             sizeof(remaddr_old));
-            printf("%zd bytes sent to %s:%d\n", result, str, remaddr_old.sin_port);
+            printf("%zd bytes sent to %s:%d\n", result, str, ntohs(remaddr_old.sin_port));
         } else {
             printf("got message from internet\n");
             struct map_entry *entry;
