3b2b9922ec5b8a329a0fea5223d9d7f34f530294
Johnson Li <johnsonli1993@gmail.com>
2018-03-25 13:59:41 +0300

    run client as sudo

--- scripts/conduct_exp1_miss.sh
+++ scripts/conduct_exp1_miss.sh
@@ -8,7 +8,7 @@ echo repeat: ${repeat}
 
 for i in `seq ${repeat}`
 do
-    output=`./server/client cdn${i}.xuebing.name`
+    output=`sudo ./server/client cdn${i}.xuebing.name`
     cost=`echo "${output}"| grep cost| egrep -o '[0-9.]+'`
     dns_cost=`echo "${output}"| grep 'DNS delay'| egrep -o '[0-9.]+'`
     dns_delay=${dns_delay},${dns_cost}
