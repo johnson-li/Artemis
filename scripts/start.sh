e7d327c3f256cdc637e8f883963200fa0cc41e46
Johnson Li <johnsonli1993@gmail.com>
2018-12-25 20:04:54 +0200

    serve on 0.0.0.0

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,11 +5,11 @@ source ~/env
 case ${ROLE} in
     "balancer")
         interface=`route |grep default| head -n1| tr -s ' '| cut -d' ' -f8`
-        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer ${interface} 127.0.0.1 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
+        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
         echo `ps --ppid $! -o pid=` >> $HOME/balancer.pid
         ;;
    "server")
-        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/server 127.0.0.1 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/server.log 2>&1
+        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/server 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/server.log 2>&1
         echo `ps --ppid $! -o pid=` >> $HOME/server.pid
    ;;
 esac
