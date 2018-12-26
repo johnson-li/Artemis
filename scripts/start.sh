65667215c6f9a071ce4acc5be5ee1583ac93638f
Johnson Li <johnsonli1993@gmail.com>
2018-12-26 21:02:49 +0200

    update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ source ~/env
 case ${ROLE} in
     "balancer")
         interface=`route |grep default| head -n1| tr -s ' '| cut -d' ' -f8`
-        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
+        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer --mysql='35.228.52.213' --user='johnson' --password='welcOme0!' ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
         echo `ps --ppid $! -o pid=` >> $HOME/balancer.pid
         ;;
    "server")
