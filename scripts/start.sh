5c37ddc00ccfb68112ae60f7450bc8de575dff9e
Johnson Li <johnsonli1993@gmail.com>
2019-01-04 10:05:02 +0800

    update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ fi
 
 case ${ROLE} in
     "balancer")
-        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer --mysql=${DATABASE} --user='johnson' --password='welcOme0!' ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
+        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer --datacenter=${DATACENTER} --mysql=${DATABASE} --user='johnson' --password='welcOme0!' ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
 #        sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
         sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/proxy ${interface} > $HOME/proxy.log 2>&1
         sudo -b PYTHONPATH="$HOME/app/" python3 -m hestia.exec.measurement-server ${DATACENTER} ${DATABASE} > $HOME/measurement.log 2>&1
