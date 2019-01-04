59fe2af105eb428365b567f1f2f5f63ae67e5891
Johnson Li <johnsonli1993@gmail.com>
2019-01-04 10:12:00 +0800

    update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,6 +7,11 @@ then
     interface=`route |grep default| head -n1| tr -s ' '| cut -d' ' -f8`
 fi
 
+sudo killall balancer 2> /dev/null
+sudo killall proxy 2> /dev/null
+sudo killall server 2> /dev/null
+sudo killall python3 2> /dev/null
+
 case ${ROLE} in
     "balancer")
         sudo -b LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/balancer --datacenter=${DATACENTER} --mysql=${DATABASE} --user='johnson' --password='welcOme0!' ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert > $HOME/balancer.log 2>&1
