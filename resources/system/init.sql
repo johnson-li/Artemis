90077257b8333fccebb25f19fcbed6ffcae5bcc8
Johnson Li <johnsonli1993@gmail.com>
2019-01-15 14:42:18 +0800

    update server name

--- resources/system/init.sql
+++ resources/system/init.sql
@@ -1,7 +1,7 @@
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'ser0', 'dc1', '173.16.156.100', 1);
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'ser1', 'dc1', '173.16.156.100', 2);
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'ser5', 'dc2', '174.16.156.100', 1);
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'ser6', 'dc2', '174.16.156.100', 2);
+insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv0', 'dc1', '173.16.156.100', 1);
+insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv1', 'dc1', '173.16.156.100', 2);
+insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv5', 'dc2', '174.16.156.100', 1);
+insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv6', 'dc2', '174.16.156.100', 2);
 
 insert into clients (ip) values ('195.148.125.212');
 
