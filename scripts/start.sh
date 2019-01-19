600e8a07e32323bed1c2372f5b2c863a8e43f272
Johnson Li <johnsonli1993@gmail.com>
2019-01-19 10:18:58 +0800

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,13 +20,13 @@ done
 
 case ${ROLE} in
     "balancer")
-        tmux send-key -t main:0 'source ~/env'
+        tmux send-key -t main:0 'source ~/env' Enter
         tmux send-key -t main:0 'sudo LD_LIBRARY_PATH=$HOME/app/bin ~/app/bin/balancer --datacenter=${DATACENTER} --mysql=${DATABASE} --user=johnson --password="welcOme0\!" ${interface} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert' Enter
-        tmux send-key -t main:1 'source ~/env'
+        tmux send-key -t main:1 'source ~/env' Enter
         tmux send-key -t main:1 'sudo PYTHONPATH="$HOME/app/" python3 -m hestia.exec.measurement-server ${DATACENTER} ${DATABASE}' Enter
         ;;
     "server")
-        tmux send-key -t main:0 'source ~/env'
+        tmux send-key -t main:0 'source ~/env' Enter
         tmux send-key -t main:0 'sudo LD_LIBRARY_PATH="$HOME/app/bin" ~/app/bin/server --interface=${interface} --unicast=${unicast} 0.0.0.0 4433 ~/app/keys/server.key ~/app/keys/server.cert' Enter
    ;;
 esac
