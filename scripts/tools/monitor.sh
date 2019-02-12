a0122f0bf232eef20099803b97087e16278da148
Johnson Li <johnsonli1993@gmail.com>
2019-02-13 00:00:30 +0800

    monitor cpu usage

--- scripts/tools/monitor.sh
+++ scripts/tools/monitor.sh
@@ -6,9 +6,11 @@ programme="$1"
 
 pid=`pidof ${programme}`
 
-ts=`echo $(($(date +%s%N)/1000000))`
-
-memory=`sudo pmap ${pid}| tail -n1| egrep -o '[0-9]+'`
-
-echo "${ts} ${memory}" >> "data/${programme}-memory"
-
+while 1; do
+    ts=`echo $(($(date +%s%N)/1000000))`
+    memory=`sudo pmap ${pid}| tail -n1| egrep -o '[0-9]+'`
+    cpu=`ps -eo pcpu,args -q ${pid}| tail -n1 | cut -d' ' -f2`
+    echo "${ts} ${memory}" >> "data/${programme}-memory"
+    echo "${ts} ${cpu}" >> "data/${programme}-cpu"
+    sleep .01
+done
