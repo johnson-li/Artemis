683abc3b75bd8c0f965b3e57b8d2764183c86e28
Johnson Li <johnsonli1993@gmail.com>
2019-02-15 16:19:29 +0800

    test inter-dc forwarding

--- resources/system/init.sql
+++ resources/system/init.sql
@@ -9,7 +9,7 @@ insert into deployment (datacenter, domain, loadbalancer) values ('dc1', 'site1.
 insert into deployment (datacenter, domain, loadbalancer) values ('dc2', 'site1.xuebingli.com', 'bl5');
 
 insert into measurements (dc, client, latency, ts) values ('dc1', '170.16.156.1', 10, 100);
-insert into measurements (dc, client, latency, ts) values ('dc2', '170.16.156.1', 100, 100);
+insert into measurements (dc, client, latency, ts) values ('dc2', '170.16.156.1', 1, 100);
 insert into measurements (dc, client, latency, ts) values ('dc1', '171.16.156.1', 10, 100);
 insert into measurements (dc, client, latency, ts) values ('dc2', '171.16.156.1', 100, 100);
 insert into measurements (dc, client, latency, ts) values ('dc1', '173.16.156.1', 10, 100);
