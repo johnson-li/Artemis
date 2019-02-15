4c18b4a5d24c8015a7aeff6a23e1237384cceaa3
Johnson Li <johnsonli1993@gmail.com>
2019-02-15 08:31:53 +0800

    update init.sql

--- resources/system/init.sql
+++ resources/system/init.sql
@@ -10,5 +10,9 @@ insert into deployment (datacenter, domain) values ('dc2', 'site1.xuebingli.com'
 
 insert into measurements (dc, client, latency, ts) values ('dc1', '170.16.156.1', 10, 100);
 insert into measurements (dc, client, latency, ts) values ('dc2', '170.16.156.1', 100, 100);
+insert into measurements (dc, client, latency, ts) values ('dc1', '171.16.156.1', 10, 100);
+insert into measurements (dc, client, latency, ts) values ('dc2', '171.16.156.1', 100, 100);
 insert into measurements (dc, client, latency, ts) values ('dc1', '173.16.156.1', 10, 100);
 insert into measurements (dc, client, latency, ts) values ('dc2', '173.16.156.1', 100, 100);
+insert into measurements (dc, client, latency, ts) values ('dc1', '174.16.156.1', 10, 100);
+insert into measurements (dc, client, latency, ts) values ('dc2', '174.16.156.1', 100, 100);
