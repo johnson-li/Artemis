d0806842f1d3ec5aeea220416437de52200d3c16
Johnson Li <johnsonli1993@gmail.com>
2019-02-27 15:38:23 +0800

    update init.sql

--- hestia/data/benchmark.py
+++ hestia/data/benchmark.py
@@ -1,3 +1,5 @@
+import os.path
+
 from numpy import percentile
 
 from hestia import RESOURCE_PATH, MATLAB_PATH
@@ -7,7 +9,7 @@ OUTPUT_PATH = MATLAB_PATH + '/benchmark.m'
 
 
 def latency():
-    end = 11
+    end = 101
     handshake_latencies = [[] for _ in range(end)]
     transfer_latencies = [[] for _ in range(end)]
     client_mems = [0 for _ in range(end)]
@@ -27,28 +29,39 @@ def latency():
                         transfer_latencies[i].append(l)
                     else:
                         print("Unexpected line: " + line)
-        with open(SID_PATH + '/dir-c%d.mem' % i) as f:
-            mem = int(f.read())
-            client_mems[i] = mem
+        if os.path.isfile(SID_PATH + '/dir-c%d.mem' % i):
+            with open(SID_PATH + '/dir-c%d.mem' % i) as f:
+                mem = int(f.read())
+                client_mems[i] = mem / 1024
         with open(SID_PATH + '/dir-s%d.mem' % i) as f:
             mem = int(f.read())
-            server_mems[i] = mem
-        with open(SID_PATH + '/dir-c%d.cpu' % i) as f:
-            max_cpu = 0
-            for line in f.readlines():
-                if line.strip():
-                    cpu = float(line.strip())
-                    if cpu > max_cpu:
-                        max_cpu = cpu
-            client_cpus[i] = max_cpu
-        with open(SID_PATH + '/dir-s%d.cpu' % i) as f:
-            max_cpu = 0
-            for line in f.readlines():
-                if line.strip():
-                    cpu = float(line.strip())
-                    if cpu > max_cpu:
-                        max_cpu = cpu
-            server_cpus[i] = max_cpu
+            server_mems[i] = mem / 1024
+        if os.path.isfile(SID_PATH + '/dir-c%d.cpu' % i):
+            with open(SID_PATH + '/dir-c%d.cpu' % i) as f:
+                max_cpu = 0
+                for line in f.readlines():
+                    line = line.strip()
+                    if line:
+                        try:
+                            cpu = float(line)
+                            if cpu > max_cpu:
+                                max_cpu = cpu
+                        except Exception:
+                            pass
+                client_cpus[i] = max_cpu
+        if os.path.isfile(SID_PATH + '/dir-s%d.cpu' % i):
+            with open(SID_PATH + '/dir-s%d.cpu' % i) as f:
+                max_cpu = 0
+                for line in f.readlines():
+                    line = line.strip()
+                    if line and not line.startswith('C') and not line.startswith('/'):
+                        try:
+                            cpu = float(line)
+                            if cpu > max_cpu:
+                                max_cpu = cpu
+                        except Exception:
+                            pass
+                server_cpus[i] = max_cpu
     print('hs = %s / 1000;' % str([percentile(handshake_latencies[i], 50) for i in range(1, end)]))
     print('tf = %s / 1000;' % str([percentile(transfer_latencies[i], 50) for i in range(1, end)]))
     print('client mems = %s' % client_mems[1:])
@@ -57,7 +70,42 @@ def latency():
     print('server cpus = %s' % client_cpus[1:])
     print('bar([hs; tf]\');')
     with open(OUTPUT_PATH, 'w') as f:
-        f.write('0')
+        lines = [
+            # =====latencies=====
+            'hs = %s / 1000;' % str([percentile(handshake_latencies[i], 50) for i in range(1, end)]),
+            'tf = %s / 1000;' % str([percentile(transfer_latencies[i], 50) for i in range(1, end)]),
+            "bar([hs; tf]');",
+            "xlabel('Number of connections');",
+            "ylabel('Latency (ms)')",
+            "set(gca,'FontSize',20)",
+            "pbaspect([2 1 1])",
+            "axis([1 100 0 600])",
+            "legend('handshake latency', 'transport latency');",
+            "print('figures/direct-latency','-depsc');",
+            # =====cpu=====
+            "cpu_cli = %s;" % client_cpus[1:],
+            "cpu_ser = %s;" % server_cpus[1:],
+            "bar([cpu_cli; cpu_ser]');",
+            "xlabel('Number of connections');",
+            "ylabel('CPU usage (%)')",
+            "set(gca,'FontSize',20)",
+            "pbaspect([2 1 1])",
+            "axis([1 100 0 13])",
+            "legend('client', 'server');",
+            "print('figures/direct-cpu','-depsc');",
+            # =====memory=====
+            "mem_cli = %s;" % client_mems[1:],
+            "mem_ser = %s;" % server_mems[1:],
+            "bar([mem_cli; mem_ser]');",
+            "xlabel('Number of connections');",
+            "ylabel('Memory (MB)')",
+            "pbaspect([2 1 1])",
+            "axis([1 100 25 42])",
+            "set(gca,'FontSize',20)",
+            "legend('client', 'server');",
+            "print('figures/direct-memory','-depsc');",
+        ]
+        f.writelines([l + '\n' for l in lines])
 
 
 def main():
--- resources/system/init.sql
+++ resources/system/init.sql
@@ -1,12 +1,12 @@
 insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv0', 'dc1', '173.16.156.100', 1);
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv1', 'dc1', '173.16.156.100', 2);
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv5', 'dc2', '174.16.156.100', 1);
-insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv6', 'dc2', '174.16.156.100', 2);
+--insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv1', 'dc1', '173.16.156.100', 2);
+--insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv5', 'dc2', '174.16.156.100', 1);
+--insert into intra (domain, server, datacenter, sid, weight) values ('site1.xuebingli.com', 'sv6', 'dc2', '174.16.156.100', 2);
 
 insert into clients (ip) values ('195.148.125.212');
 
 insert into deployment (datacenter, domain, loadbalancer) values ('dc1', 'site1.xuebingli.com', 'bl0');
-insert into deployment (datacenter, domain, loadbalancer) values ('dc2', 'site1.xuebingli.com', 'bl5');
+--insert into deployment (datacenter, domain, loadbalancer) values ('dc2', 'site1.xuebingli.com', 'bl5');
 
 insert into measurements (dc, client, latency, ts) values ('dc1', '170.16.156.1', 10, 100);
 insert into measurements (dc, client, latency, ts) values ('dc2', '170.16.156.1', 1, 100);
