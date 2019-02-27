a822ee7615a17717bd89459bbb6857ef18662516
Johnson Li <johnsonli1993@gmail.com>
2019-02-27 13:40:00 +0800

    update get_data.sh

--- resources/data/get_data.sh
+++ resources/data/get_data.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+ssh workstation-br "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no johnson@173.16.156.101:'~/data/sid/*' ~/data/sid"
 rsync -r workstation-br:"~/data/sid" .
