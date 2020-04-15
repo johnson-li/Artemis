f30ec9a67de8bebaae5b19d00fa8ef8bd8aae9d5
Johnson Li <johnsonli1993@gmail.com>
2020-04-16 01:35:49 +0300

    gcp clients

---
+++ experiment/client/machines/gcp/start.py
@@ -0,0 +1,23 @@
+from experiment.gcloud.gce_utils import create_instance, ZONES
+
+ZONES_ALL = ['us-east1-c', 'us-east4-c', 'us-central1-c', 'us-west1-c', 'us-west2-c', 'us-west3-c',
+             'europe-west1-c', 'europe-west2-c', 'europe-west3-c', 'europe-west4-c', 'europe-west6-c',
+             'europe-north1-c',
+             'asia-east1-c', 'asia-east2-c', 'asia-southeast1-c', 'asia-northeast1-c', 'asia-northeast2-c',
+             'asia-northeast3-c', 'asia-south1-c', 'australia-southeast1-c',
+             'southamerica-east1-c', 'northamerica-northeast1-c']
+
+zones = []
+for z in ZONES_ALL:
+    if z not in ZONES:
+        zones.append(z)
+
+
+def main():
+    print('Zones: %s' % zones)
+    for zone in zones:
+        create_instance(zone, 'client-%s' % zone)
+
+
+if __name__ == '__main__':
+    main()
