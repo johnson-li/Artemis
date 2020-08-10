782cf93dd4721fe4bb0641fc976842bcf53eaf74
Xuebing Li <johnsonli1993@gmail.com>
2020-08-10 10:18:04 +0300

    update exp scripts

--- experiment/gcloud/data2/start_experiment.sh
+++ experiment/gcloud/data2/start_experiment.sh
@@ -2,21 +2,13 @@ date > ~/start_experiment.sh.start_ts
 
 tmux has-session -t main 2> /dev/null; if [[ $? == 0  ]]; then tmux kill-session -t main; fi
 tmux new-session -ds main
-for i in `seq 4`
+for i in $(seq 4)
 do
-    tmux new-window -t main:${i}
+    tmux new-window -t main:"$i"
 done
 
-hostname=`hostname`
-role=${hostname:`expr ${#hostname} - 6`:6}
-case $role in
-    "router")
-        tmux send-key -t main:0 '~/data/start_balancer.sh' Enter
-        tmux send-key -t main:1 'mysql -ujohnson -pjohnson serviceid_db' Enter
-        ;;
-    "server")
-        tmux send-key -t main:0 '~/data/start_server.sh' Enter
-        ;;
-esac
+tmux send-key -t main:0 '~/data2/start_balancer.sh' Enter
+tmux send-key -t main:1 '~/data2/start_server.sh' Enter
+tmux send-key -t main:2 'mysql -u johnson -p johnson serviceid_db' Enter
 
 date > ~/start_experiment.sh.end_ts
