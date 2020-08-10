0b58ff5e8212fe6a15ec24ef56b08224feeea495
Xuebing Li <johnsonli1993@gmail.com>
2020-08-10 10:48:06 +0300

    fix start_server.sh

--- experiment/gcloud/data2/start_server.sh
+++ experiment/gcloud/data2/start_server.sh
@@ -1,6 +1,6 @@
 date > ~/start_server.sh.start_ts
 
 unicast=$(python3 -c 'import socket; import os; import json; machines=json.load(open("machine.json")); print(machines[socket.gethostname()]["external_ip1"])')
-sudo LD_LIBRARY_PATH=~/data2 ~/data2/server_transport --interface=ens4 --unicast="$unicast" 0.0.0.0 4434 ~/data/server.key ~/data/server.crt
+sudo LD_LIBRARY_PATH=~/data2 ~/data2/server_transport --interface=ens4 --unicast="$unicast" 0.0.0.0 4434 ~/data2/server.key ~/data2/server.crt
 
 date > ~/start_server.sh.end_ts
