ca198d90c1e17a18c39337ff707440abb4f76ce5
Johnson <johnsonli1993@gmail.com>
2020-09-20 09:03:32 +0000

    update

---
+++ all_in_one.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# conda activate dev
+
+# python -m experiment.gcloud.main2
+# sleep 20
+python -m experiment.gcloud.restart2
+
+echo The server is ready.
+
+for i in `seq 1`
+do
+    python -m experiment.client.machines.aws.start --range $(((i-1)*4)),$((i*4))
+    sleep 10
+    python -m experiment.client.main
+    item_count=$(mysql -ujohnson -pjohnson -D serviceid_db -sN -e 'select count(*) from transfer_time;')
+    while (( $item_count < $((i*4*5)) ))
+    do
+        sleep 10
+        item_count=$(mysql -ujohnson -pjohnson -D serviceid_db -sN -e 'select count(*) from transfer_time;')
+    done
+    python -m experiment.client.machines.aws.cleanup
+    sleep 5
+done
+
